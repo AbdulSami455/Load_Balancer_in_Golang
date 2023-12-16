d0dfe15a7f85140b16de49c9216b98317fedc0ee
AbdulSami455 <msami.bese22seecs@student.nust.edu.pk>
2023-12-16 20:00:48 +0000

    Structure Defined

--- main.go
+++ main.go
@@ -2,7 +2,16 @@ package main
 
 import (
 	"fmt"
+  "net/http/httputil"
+  
 )
+
+type simpleserver struct{
+  addr string 
+  proxy "httputil.ReverseProxy"  
+}
+
+
 
 func main() {
 	fmt.Println("We want to Build Golang Load Balance")
