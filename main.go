47cf425df7ac98cfbfcbdcd5df115b522f4f22a3
AbdulSami455 <msami.bese22seecs@student.nust.edu.pk>
2023-12-16 20:14:15 +0000

    Creating Functions and Structs for ReverseProxy

--- main.go
+++ main.go
@@ -11,8 +11,21 @@ type simpleserver struct{
   proxy "httputil.ReverseProxy"  
 }
 
+func newsimpleserver(addr string) *simpleserver{
+  serverUrl, err:=url.Parse(addr)
+  handleErr(err)
 
+  return &simpleserver{
+    addr: addr,
+    proxy: httputil.NewSingleHostReverseProxy(serverUrl),
+}
+}
 
+  func handleErr(err error){
+  if err!=nil{
+  fmt.Println("Error: %v/n ", err)
+}    
+  }   
 func main() {
 	fmt.Println("We want to Build Golang Load Balance")
 }
